880cd1619bc9d7e66af1f7d7a49e2df104c007e7
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-08-01 19:48:34 -0300

    fix: arruma bug do jwt que fazia o teste não passo no github

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -1,12 +1,7 @@
-import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import IUser from '../interfaces/IUser';
 
-dotenv.config();
-
 export default (payload: IUser): string => {
-  const secret = process.env.JWT_SECRET as string;
-
-  const token = jwt.sign(payload, secret, { expiresIn: '7d', algorithm: 'HS256' });
+  const token = jwt.sign(payload, 'secret', { expiresIn: '7d', algorithm: 'HS256' });
   return token;
 };
