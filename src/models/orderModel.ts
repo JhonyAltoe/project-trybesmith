ec46087abd70d1a501597922ec594f62237724c1
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-08-01 19:18:54 -0300

    fear: adiciona rota que pega todas as orders

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -13,19 +13,11 @@ export default class OrderModel {
   }
 
   public async listAllOrders(): Promise<IOrderArr[]> {
-    // const query2 = `
-    // SELECT O.id, O.userId, JSON_ARRAYAGG(P.id) AS productsIds FROM Trybesmith.Orders AS O
-    // INNER JOIN Trybesmith.Products AS P
-    // ON O.id = P.orderId
-    // WHERE P.orderId IS NOT null
-    // GROUP BY O.id`;
-    
     const query = `
       SELECT O.id, O.userId, P.id AS productsIds FROM Trybesmith.Orders AS O
       INNER JOIN Trybesmith.Products AS P
       ON O.id = P.orderId
-      WHERE P.orderId IS NOT null
-      ORDER BY O.id`;
+      WHERE P.orderId IS NOT null`;
     
     const [orders] = await this
       .connection.query<RowDataPacket[]>(query);
